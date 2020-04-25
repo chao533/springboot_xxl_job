34d9adf6e0a170531bf5b3ffca0e0fc732e04126
Administrator <Administrator@PC-201906261744>
2020-04-25 20:18:37 +0800

    edit job

---
+++ src/main/java/com/kang/common/constans/JobConstants.java
@@ -0,0 +1,27 @@
+package com.kang.common.constans;
+
+import cn.hutool.setting.Setting;
+
+public class JobConstants {
+
+	public static final String APPLICATION = "application.properties";
+
+	public interface AppKey {
+		String ADDRESSES = "xxl.job.admin.addresses";
+		String USER = "admin";
+		String PASSWORD = "123456";
+	}
+
+	public static String getProp(String file, String key) {
+		Setting setting = new Setting(file);
+		return setting.get(key);
+	}
+	
+	public static final String JOB_LOGIN = getProp(JobConstants.APPLICATION, JobConstants.AppKey.ADDRESSES) + "/login";
+	
+	public static final String JOB_ADD = getProp(JobConstants.APPLICATION, JobConstants.AppKey.ADDRESSES) + "/jobinfo/add";
+	
+	public static final String JOB_PAGE_LIST = getProp(JobConstants.APPLICATION, JobConstants.AppKey.ADDRESSES) + "/jobinfo/pageList";
+
+	public static final String JOB_START = getProp(JobConstants.APPLICATION, JobConstants.AppKey.ADDRESSES) + "/jobinfo/start";
+}
--- src/test/java/com/chao/job/JobTest.java
+++ src/test/java/com/chao/job/JobTest.java
@@ -5,22 +5,25 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import com.kang.common.constans.JobConstants;
+
 import cn.hutool.core.lang.Console;
+import cn.hutool.core.lang.Dict;
 import cn.hutool.core.lang.TypeReference;
 import cn.hutool.core.map.MapUtil;
-import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
-import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
 
 public class JobTest {
 	
-	
-
+	/**
+	 * <p>Title: testAddJob</p>
+	 * <p>Description: 添加任务</p>
+	 * @param
+	 */
 	@Test
-	public void addJob() {
-		
+	public void testAddJob() {
 		Map<String,Object> formMap = MapUtil.builder(new HashMap<String,Object>())
 			.put("jobGroup", 1)
 			.put("jobDesc", "测试任务2")
@@ -39,10 +42,10 @@ public class JobTest {
 			.put("glueSource", "")
 			.build();
 		
-		HttpResponse httpResponse = HttpRequest.post("http://127.0.0.1:8080/xxl-job-admin/jobinfo/add")
+		HttpResponse httpResponse = HttpRequest.post(JobConstants.JOB_ADD)
 			.contentType("application/x-www-form-urlencoded; charset=UTF-8")
 			.header("cache-control", "no-cache")
-			.cookie("XXL_JOB_LOGIN_IDENTITY=6333303830376536353837616465323835626137616465396638383162336437")
+			.cookie(toLogin())
 			.form(formMap)
 			.execute();
 		
@@ -51,83 +54,33 @@ public class JobTest {
 		Console.log(httpResponse.body());
 	}
 	
+	/**
+	 * <p>Title: testPageList</p>
+	 * <p>Description: 查看任务列表</p>
+	 * @param
+	 */
 	@Test
-	public void addJob2() {
-		
-		Map<String,Object> formMap = MapUtil.builder(new HashMap<String,Object>())
-				.put("jobGroup", 1)
-				.put("jobDesc", "测试任务2")
-				.put("executorRouteStrategy", "FIRST")
-				.put("jobCron", "0 0 0 * * ? *")
-				.put("glueType", "BEAN")
-				.put("executorHandler", "demoJobHandler")
-				.put("executorBlockStrategy", "SERIAL_EXECUTION")
-				.put("childJobId", 1)
-				.put("executorTimeout", 0)
-				.put("executorFailRetryCount", 0)
-				.put("author", "ck")
-				.put("alarmEmail", "")
-				.put("executorParam", "{\"username\":\"king\",\"password\":\"123456\"}")
-				.put("glueRemark", "GLUE代码初始化")
-				.put("glueSource", "")
-				.build();
-		String res = HttpUtil.post("http://192.168.0.155:8080/xxl-job-admin/jobinfo/add", formMap);
-		Console.log(res);
-	}
-	
-	@Test
-	public void test3() {
-		String url = "http://192.168.0.206/spad-login/common/userLogin";
-		Map<String,Object> paramMap = MapUtil.builder(new HashMap<String,Object>()).put("username", "king").put("password", "123456").build();
-		String body = JSONUtil.toJsonStr(paramMap);
-//		String res = HttpUtil.post(url, body);
-//		Console.log(res);
-		
-		HttpResponse httpResponse = HttpRequest.post(url)
-			.contentType("application/json")
-			.body(body)
-			.execute();
-		
-		Console.log(httpResponse.body());
-		Map<String,Object> bodyMap = JSONUtil.toBean(httpResponse.body(), new TypeReference<Map<String,Object>>() {}, true);
-		Console.log(bodyMap);
-	}
-	
-	private static final String LOCAL_URL = "http://127.0.0.1:8080/xxl-job-admin/jobinfo/pageList";
-	
-	private static final String DEV_URL = "http://116.62.195.201:8080/xxl-job-admin/jobinfo/pageList";
-	
-	String cookie = "";
-	@Test
-	public void test4() {
-		
-		Map<String,Object> paramMap = MapUtil.builder(new HashMap<String,Object>()).put("jobGroup", 2).put("start", 0).put("length", 10).build();
-		//String body = JSONUtil.toJsonStr(paramMap);
-//		String res = HttpUtil.post(url, body);
-//		Console.log(res);
-		if(StrUtil.isNotBlank(cookie)) {
-			cookie = toLogin();
-		}
-		HttpResponse httpResponse = HttpRequest.post(DEV_URL)
+	public void testPageList() {
+		Map<String,Object> paramMap = Dict.create().set("jobGroup", 1).set("start", 0).set("length", 10);
+		HttpResponse httpResponse = HttpRequest.post(JobConstants.JOB_PAGE_LIST)
 				.contentType("application/x-www-form-urlencoded; charset=UTF-8")
-				.cookie(cookie)
+				.cookie(toLogin())
 				.form(paramMap)
 				.execute();
-		if(httpResponse.getStatus() == 302) {
-			cookie = toLogin();
-			test4();
-		}
-		Console.log(httpResponse);
+		Console.log("httpResponse:{}",httpResponse);
 		Map<String,Object> bodyMap = JSONUtil.toBean(httpResponse.body(), new TypeReference<Map<String,Object>>() {}, true);
 		Console.log(bodyMap);
 	}
 	
 	
-	
+	/**
+	 * <p>Title: toLogin</p>
+	 * <p>Description: xxl-job登录</p>
+	 * @param @return
+	 */
 	public String toLogin() {
-		Map<String,Object> paramMap = MapUtil.builder(new HashMap<String,Object>()).put("userName", "admin").put("password", "123456").build();
-		
-		HttpResponse httpResponse = HttpRequest.post("http://127.0.0.1:8080/xxl-job-admin/login")
+		Map<String,Object> paramMap = Dict.create().set("userName", JobConstants.AppKey.USER).set("password", JobConstants.AppKey.PASSWORD);
+		HttpResponse httpResponse = HttpRequest.post(JobConstants.JOB_LOGIN)
 				.form(paramMap)
 				.execute();
 		if(httpResponse.getStatus() == 200) {
